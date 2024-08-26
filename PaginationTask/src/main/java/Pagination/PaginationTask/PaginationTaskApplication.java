2901cac16b5bdf37de71b297d745a17cc9085eb6
Prasanthsivaselva <Prasanthsivaselva@prasanth1725>
2024-08-26 11:22:08 +0530

    demo changes

---
+++ PaginationTask/src/main/java/Pagination/PaginationTask/PaginationTaskApplication.java
@@ -0,0 +1,13 @@
+package Pagination.PaginationTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationTaskApplication.class, args);
+	}
+
+}
---
+++ PaginationTask/src/test/java/Pagination/PaginationTask/PaginationTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package Pagination.PaginationTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
