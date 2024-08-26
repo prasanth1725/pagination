5f00346caebcd91aa35dccb70fcd472462023d36
Prasanthsivaselva <Prasanthsivaselva@prasanth1725>
2024-08-26 11:39:41 +0530

    sysout

--- PaginationTask/src/main/java/Pagination/PaginationTask/PaginationTaskApplication.java
+++ PaginationTask/src/main/java/Pagination/PaginationTask/PaginationTaskApplication.java
@@ -8,6 +8,8 @@ public class PaginationTaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaginationTaskApplication.class, args);
+		
+		System.out.println("hlo");
 	}
 
 }
